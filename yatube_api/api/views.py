64e220b801d114a1d5de47ca0138be5073c3967d
rete11 <126920469+rete11@users.noreply.github.com>
2023-11-24 06:59:56 +0300

    fix

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -34,10 +34,13 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (IsAuthenticated, AuthorPermission)
 
+    def get_post(self):
+        return get_object_or_404(Post, id=self.kwargs.get("post_id"))
+
     def perform_create(self, serializer):
-        post = get_object_or_404(Post, id=self.kwargs.get("post_id"))
+        post = self.get_post()
         serializer.save(author=self.request.user, post=post)
 
     def get_queryset(self):
-        post = get_object_or_404(Post, id=self.kwargs.get("post_id"))
+        post = self.get_post()
         return post.comments.all()
